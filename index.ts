0ca3434d9ba4debb2d03a2aea27108ef5d7c5f49
lxf <623337308@qq.com>
2021-11-17 17:22:31 +0800

    #os

--- index.ts
+++ index.ts
@@ -1,41 +1,40 @@
-import os from 'os-utils'
+import osu from 'node-os-utils'
 import * as http from 'http'
 import * as socket from 'socket.io'
 import { diskinfo } from '@dropb/diskinfo';
 
+const { cpu, os, users, mem, netstat } = osu
 const server = http.createServer()
 const io = new socket.Server(server, {
-  "transports": ['websocket', 'polling']
+  transports: ['websocket', 'polling']
 });
+
 server.listen(4000, function () {
   console.log('listen on 4000');
 });
 
 io.sockets.on('connection', socket => {
-  socket.emit("connected", "连接成功")
   console.log("连接成功")
-  socket.on("disconnect", () => {
-    console.log("disconnect")
+  socket.emit("connected", {
+    type: os.type(), // 平台类型 linux
+    arch: os.arch(), // x64 x32
+    model: cpu.model() // Intel(R) Xeon(R) Platinum 8255C CPU @2.50GHz
   })
+  socket.on("disconnect", () => { console.log("连接断开") })
 })
 
 function sleep(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)) }
 
 async function start(): Promise<void> {
-  let cpuUse = 0
-  os.cpuUsage(i => cpuUse = i * 100);
-  // 获取当前可用内存
-  const freemem = (os.freemem() / 1024).toFixed(2)
-  // 获取总内存
-  const totalmem = (os.totalmem() / 1024).toFixed(2)
-  // 获取空闲内存的百分比
-  const freememPercentage = os.freememPercentage()
-  // 获取系统已运行的毫秒数。
-  const sysUptime = (os.sysUptime() / 86400).toFixed(2)
-  // 获取 1、5 或 15 分钟的平均负载
-  const loadavg = [os.loadavg(1), os.loadavg(5), os.loadavg(15)]
-  // sockets
-  io.sockets.emit("systemInfo", { freemem, totalmem, freememPercentage, sysUptime, loadavg, diskinfo: await diskinfo() })
+  io.sockets.emit("systemInfo", {
+    cpuUse: await cpu.usage(),
+    openedCount: await users.openedCount(),
+    uptime: (os.uptime() / 86400).toFixed(2),
+    loadavg: [(cpu.loadavgTime(1)).toFixed(2), (cpu.loadavgTime(5)).toFixed(2), (cpu.loadavgTime(15)).toFixed(2)],
+    mem: await mem.info(),
+    netstat: await netstat.inOut(),
+    diskinfo: await diskinfo()
+  })
   await sleep(1000)
   start()
 }
